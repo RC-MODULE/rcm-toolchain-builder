d98d02d43128e5949425e91a2594b6510ff51725
Andrew Andrianov <andrew@ncrmnt.org>
2016-06-23 23:01:26 +0300

    Add symlink2copy script
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ symlink2copy.sh
@@ -0,0 +1,16 @@
+  for link in `find . -type l`; do
+    dir=$(dirname "$link")
+    reltarget=$(readlink "$link")
+    if [[ "$reltarget" == "/*" ]]; then
+        abstarget=.$reltarget
+    else
+        abstarget=$dir/$reltarget
+    fi
+
+    rm -f "$link"
+    cp -af "$abstarget" "$link" || {
+        # on failure, restore the symlink
+        rm -rfv "$link"
+        ln -sfv "$reltarget" "$link"
+    }
+  done
